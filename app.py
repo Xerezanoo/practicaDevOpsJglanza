d06dd7cc20f9afeef73d59d0656d1f30ee566fb0
Xerezanoo <juangarcialanza120805@gmail.com>
2026-04-23 19:43:49 +0200

    Añadir app.py V2 - Errores linting corregidos

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 # app.py
-""" Docstring del módulo """
-def suma(a: int, b: int) -> int:
-    """ docstring function
-    # debe dar errores en linter
-    """
-    x = "no se usa"
-    return a+ b
+"""Módulo para realizar operaciones matemáticas simples."""
 
+def suma(a: int, b: int) -> int:
+    """
+    Calcula la suma de dos números enteros.
+
+    Args:
+        a: El primer número.
+        b: El segundo número.
+
+    Returns:
+        La suma de los dos parámetros.
+    """
+    return a + b
